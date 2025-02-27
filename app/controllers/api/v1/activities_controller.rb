d6476991f37ec3c0bc40d91a3f37bacf32437efa
Tanner <tannermk92@gmail.com>
2025-02-27 20:28:00 +0000

    activities cont

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -4,6 +4,26 @@ class Api::V1::ActivitiesController < ApplicationController
     render json: activities, status: 200
   end
 
+  def create
+    t = Time.now
+    activity = Activity.new(
+      starttime: t,
+      endtime: t + 10,
+      description: "walk to park"
+    )
+    if activity.save
+      render json: activity, status: 200
+    else
+      render json: { error: "Error saving activity" }
+    end
+  end
+
   def show
+    activity = Activity.find_by(id: params[:id])
+    if activity
+      render json: activity, status: 200
+    else
+      render json: { error: "No activity found" }
+    end
   end
 end
